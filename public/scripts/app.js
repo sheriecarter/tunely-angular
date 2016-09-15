27c829cca08d42300a289f3064bb3a2cf7bbd5c4
Sherie Carter <sherie1.carter@gmail.com>
2016-09-15 14:48:10 -0700

    solutions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,37 +1,35 @@
-/* CLIENT-SIDE JS
- *
- * This is your main angular file. Edit as you see fit.
- *
- */
-
 angular
   .module('tunely', [])
   .controller('AlbumsIndexController', AlbumsIndexController);
-  // ^ the first argument is a string naming the controller,
-  // the second argument is a function that defines the capacities
-  // of the controller.
 
-function AlbumsIndexController () {
+AlbumsIndexController.$inject = ['$http'];
+
+function AlbumsIndexController ($http) {
   var vm = this;
   vm.newAlbum = {};
-
   vm.newAlbum = {
-      name: 'Viva Hate',
-      artistName: 'Morrissey'
+    name: 'Viva Hate',
+    artistName: 'Morrissey'
   };
 
-  vm.albums = [
-    {
-      name: 'Coming Home',
-      artistName: 'Leon Bridges'
-    },
-    {
-      name: 'Are We There',
-      artistName: 'Sharon Van Etten'
-    },
-    {
-      name: 'The Queen is Dead',
-      artistName: 'The Smiths'
-    }
-  ];
+  $http({
+    method: 'GET',
+    url: '/api/albums'
+  }).then(function successCallback(response) {
+    vm.albums = response.data;
+  }, function errorCallback(response) {
+    console.log('There was an error getting the data', response);
+  });
+
+  vm.createAlbum = function () {
+    $http({
+      method: 'POST',
+      url: '/api/albums',
+      data: vm.newAlbum,
+    }).then(function successCallback(response) {
+      vm.albums.push(response.data);
+    }, function errorCallback(response) {
+      console.log('There was an error posting the data', response);
+    });
+  }
 }
